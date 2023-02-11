5667a737d801fdc34788544efdf55efe9af325ad
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-02-11 11:31:36 +0100

    fixes

--- src/Component/Nav.jsx
+++ src/Component/Nav.jsx
@@ -11,7 +11,7 @@ const handleshownav=()=>{
     <div className="icon">
       <img src="/media/logo.svg" alt="" />
     </div>
-    <img src="/media/icon-menu.svg" className='lg:hidden block  z-[1] fixed' alt="" onClick={handleshownav}/>
+    <img src={menu?'/media/icon-menu-close.svg':'/media/icon-menu.svg'} className='lg:hidden block  z-[1]' alt="" onClick={handleshownav}/>
     <div className={`nav ${menu && 'active'}`} >
       <p>Home</p>
       <p>New</p>
