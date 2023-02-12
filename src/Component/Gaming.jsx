4f28ef589fce076818ff3a47a03735dba4bd242e
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-02-12 07:01:01 +0100

    fixes

--- src/Component/Gaming.jsx
+++ src/Component/Gaming.jsx
@@ -6,7 +6,7 @@ const Gaming = (props) => {
         <img src={props.img} alt="" className='w-[5rem]'/>
         <div className="retro">
             <h1 className='text-[#cac7ce] lg:text-[2rem] font-[600] text-[1.5rem]'>{props.num}</h1>
-            <p className='font-[700]'>{props.desc1}</p>
+            <p className='font-[700] hover:text-red-500 cursor-pointer'>{props.desc1}</p>
             <p className='text-[#cac7ce] leading-5 lg:text-[1rem] text-[.95rem]'>{props.desc2}</p>
         </div>
     </div>
--- src/Component/Hydro.jsx
+++ src/Component/Hydro.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hydro = (props) => {
   return (
 <div>
-    <h1 className='text-white text-[1.5rem]'>{props.title}</h1>
+    <h1 className='text-white text-[1.5rem] hover:cursor-pointer hover:text-[#eeaa4f]'>{props.title}</h1>
     <p className='text-white opacity-[0.7] lg:text-[1rem] text-[1.3rem]'>{props.desc}</p>
 </div>
   )
